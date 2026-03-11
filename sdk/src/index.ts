9d472b7d5e79f64a6c5ca9b630739fcbd0cf66fe
Matty Hogan <matthewhogan63@gmail.com>
2026-03-11 17:08:31 -0400

    Add Subscriber alias export for backwards compatibility

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,4 +1,4 @@
 export { Lux } from "./client";
-export { LuxSubscriber } from "./subscriber";
+export { LuxSubscriber, LuxSubscriber as Subscriber } from "./subscriber";
 export { LuxError, LuxConnectionError } from "./errors";
 export type { LuxConfig, RespValue } from "./types";
